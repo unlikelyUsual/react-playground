c0caf5b0f616a5933a0d9ab8f1b66848b59f4230
Prabhat <prabhat@Prabhats-Macbook.local>
2024-05-10 11:26:37 +0530

    fix

--- client/src/service/user.tsx
+++ client/src/service/user.tsx
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store/store";
 
 export const userApi = createApi({
   //   reducerPath: "user",
@@ -7,7 +8,7 @@ export const userApi = createApi({
     baseUrl: "http://localhost:4000/",
     prepareHeaders: (headers, { getState }) => {
       // By default, if we have a token in the store, let's use that for authenticated requests
-      const token = (getState() as any).auth.token;
+      const token = (getState() as RootState).auth.token;
       if (token) {
         headers.set("authorization", `Bearer ${token}`);
       }
